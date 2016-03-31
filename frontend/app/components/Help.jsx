20b933921d0260d23b2e3123d0a397d4cf95a1bc
abvhiael crowley <abvhiael@live.ca>
2016-03-31 11:15:53 -0300

    Update Help.jsx

--- frontend/app/components/Help.jsx
+++ frontend/app/components/Help.jsx
@@ -6,9 +6,9 @@ let Help = React.createClass({
       <div className="container-fluid">
         <h1>{this.props.title}</h1>
         <section className="text-center">
-          <h3>See the <a href="https://github.com/etherex/etherex#readme" target="_blank">GitHub readme</a> for documentation</h3>
+          <h3>See the <a href="https://github.com/abvhiael/etherex#readme" target="_blank">GitHub readme</a> for documentation</h3>
           <h4>Or the <a href="https://etherex.org/faq" target="_blank">FAQ</a> on the <a href="https://etherex.org" target="_blank">website</a></h4>
-          <p>Complete dApp documentation will be here soon.</p>
+          <p>User manual documentation available at <a href="https://etherex.org/faq" target="_blank">SOIL-ex</a> website. .</p>
         </section>
       </div>
     );
